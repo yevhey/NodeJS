46eef645e4fea6da906a3d9b01ee25373685de23
Yevhenii Zhdan <yevhenii.zhdan@preply.com>
2024-01-07 22:17:38 +0200

    progress

--- app.js
+++ app.js
@@ -1,17 +1,52 @@
 const arr1 = ['dPRZxomA', '50aoPS1ZOu', 'TPGjTL8l', 'OlzF9pRSP'];
 const arr2 = ['0mHXAQJY6', 'mQueU006Is', 'cFM17Eou', 'pFUk0P3Ivo', 'Ob9NtLii', 'Jk8x5DBNxL'];
 const searchValue = '50aoPS1ZOu';
-const { Worker } = require('node:worker_threads');
+
+const { Worker, MessageChannel } = require('node:worker_threads');
+const { port1, port2 } = new MessageChannel();
+const worker1 = new Worker(
+    __filename,
+    {
+        workerData: {
+            someString: 'be ready!',
+            postPort: port1,
+        },
+        transferList: [port1],
+    },
+);
+const worker2 = new Worker(
+    './worker2.js',
+    {
+        workerData: {
+            listenPort: port2,
+        },
+        transferList: [port2],
+    },
+);
+
+worker1.postMessage('start');
+
+[worker1, worker2].forEach((worker) => {
+    worker.on('message', (message) => {
+        if(message === 'Finished') {
+            worker.terminate();
+        }
+    })
+});
 
 function workerFunction() {
-    console.log(arr);
+    const foundIndex = arr.findIndex(element => element === value);
+
+    if (foundIndex !== -1) {
+        console.log(`Element found at index ${foundIndex} of array ${arrayNumber}`);
+    } else {
+        console.log('Element not found');
+    }
 }
 
 function findElement(firstArray, secondArray, value) {
-    const workerScript1 = `console.log(typeof '${firstArray}')`;
-    // const workerScript2 = `const arr = ${secondArray.split(',')}; workerFunction();`;
-    const worker1 = new Worker(workerScript1, {eval: true});
-    // const worker2 = new Worker(workerScript2, {eval: true});
+    const workerScript1 = `const arrayNumber = 1; const value = '${value}'; const arr = '${firstArray}'.split(','); ${workerFunction} workerFunction();`;
+    const workerScript2 = `const arrayNumber = 2; const value = '${value}'; const arr = '${secondArray}'.split(','); ${workerFunction} workerFunction();`;
+    const worker1 = new Worker(workerScript1, { eval: true });
+    const worker2 = new Worker(workerScript2, { eval: true });
 }
-
-findElement(arr1, arr2, searchValue);
